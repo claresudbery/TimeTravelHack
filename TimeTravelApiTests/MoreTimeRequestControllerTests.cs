c3a022c418068fceb2e8f9b8d5409792e7b3d6da
claresudbery <clareworking@claresudbery.co.uk>
2018-10-27 19:04:35 +0100

    Put basics in place ready for unit testing

---
+++ TimeTravelApiTests/MoreTimeRequestControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimeTravelApi.Controllers;
+
+namespace TimeTravelApiTests
+{
+    [TestClass]
+    public class MoreTimeRequestControllerTests
+    {
+        [TestMethod]
+        public void GivenMoreTimeRequestedWhen20MinutesElapsedThenAlertHappens()
+        {
+            var controller = new MoreTimeRequestController();
+
+        }
+    }
+}
