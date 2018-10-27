9cf3a5845f666388df42a402963197b0c669d3f0
claresudbery <clareworking@claresudbery.co.uk>
2018-10-27 21:41:08 +0100

    Trying to get unit tests working... not succeeding yet though...

---
+++ TimeTravelApi.Tests/MoreTimeRequestControllerTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using TimeTravelApi.Controllers;
+using TimeTravelApi.Models;
+//using Microsoft.AspNetCore.Mvc;
+
+namespace Tests
+{
+    public class MoreTimeRequestControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GivenMoreTimeRequested_When20MinutesElapse_ThenAlertHappens()
+        {
+            var controller = new MoreTimeRequestController(
+                new MoreTimeRequestContext(
+                    new DbContextOptions<MoreTimeRequestContext>()));
+            //var result = controller.GetAlert(true);
+            Assert.Pass();
+        }
+    }
+}
