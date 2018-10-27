be45a50cd2aa0905719f4c53d71c0bf960d8f45a
claresudbery <clareworking@claresudbery.co.uk>
2018-10-27 22:18:48 +0100

    Added a front end in the WebApi project

--- TimeTravelApi/Startup.cs
+++ TimeTravelApi/Startup.cs
@@ -18,6 +18,8 @@ namespace TimeTravelApi
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
---
+++ TimeTravelApi/wwwroot/site.js
@@ -0,0 +1,104 @@
+const uri = 'api/moretimerequest';
+let todos = null;
+function getCount(data) {
+    const el = $('#counter');
+    let name = 'to-do';
+    if (data) {
+        if (data > 1) {
+            name = 'to-dos';
+        }
+        el.text(data + ' ' + name);
+    } else {
+        el.html('No ' + name);
+    }
+}
+
+$(document).ready(function () {
+    getData();
+});
+
+function getData() {
+    $.ajax({
+        type: 'GET',
+        url: uri,
+        success: function (data) {
+            $('#todos').empty();
+            getCount(data.length);
+            $.each(data, function (key, item) {
+                const checked = item.Expired ? 'checked' : '';
+
+                $('<tr><td><input disabled="true" type="checkbox" ' + checked + '></td>' +
+                    '<td>' + item.id + '</td>' +
+                    '<td><button onclick="editItem(' + item.id + ')">Edit</button></td>' +
+                    '<td><button onclick="deleteItem(' + item.id + ')">Delete</button></td>' +
+                    '</tr>').appendTo($('#todos'));
+            });
+
+            todos = data;
+        }
+    });
+}
+
+function addItem() {
+
+    $.ajax({
+        type: 'POST',
+        accepts: 'application/json',
+        url: uri,
+        contentType: 'application/json',
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert('Something went wrong with addItem');
+        },
+        success: function (result) {
+            getData();
+            $('#add-name').val('');
+        }
+    });
+}
+
+function deleteItem(id) {
+    $.ajax({
+        url: uri + '/' + id,
+        type: 'DELETE',
+        success: function (result) {
+            getData();
+        }
+    });
+}
+
+function editItem(id) {
+    $.each(todos, function (key, item) {
+        if (item.id === id) {
+            $('#edit-name').val(item.name);
+            $('#edit-id').val(item.id);
+            $('#edit-isComplete')[0].checked = item.isComplete;
+        }
+    });
+    $('#spoiler').css({ 'display': 'block' });
+}
+
+$('.my-form').on('submit', function () {
+    const item = {
+        'name': $('#edit-name').val(),
+        'isComplete': $('#edit-isComplete').is(':checked'),
+        'id': $('#edit-id').val()
+    };
+
+    $.ajax({
+        url: uri + '/' + $('#edit-id').val(),
+        type: 'PUT',
+        accepts: 'application/json',
+        contentType: 'application/json',
+        data: JSON.stringify(item),
+        success: function (result) {
+            getData();
+        }
+    });
+
+    closeInput();
+    return false;
+});
+
+function closeInput() {
+    $('#spoiler').css({ 'display': 'none' });
+}
