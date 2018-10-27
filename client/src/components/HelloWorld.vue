9380f18494ea0c746e92128fa636b7fb0efcbad0
Cynthia Lee <cynthialee@Cynthias-MBP-3.example.com>
2018-10-27 15:41:49 +0100

    First client commit

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button @click="TravelClicked">Travel in Time!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Time Travel'
+    }
+  },
+  methods: {
+    TravelClicked () {
+      console.log("Works!")
+    }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
