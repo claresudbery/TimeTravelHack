2a22f5e74fb2363d22da3cdea7e97447093e5987
Cynthia Lee <cynthia.lee@gmx.de>
2018-10-28 13:07:57 +0000

    fix alert bug and uplaod some demo code

--- TimeTravelApi/wwwroot/site.js
+++ TimeTravelApi/wwwroot/site.js
@@ -100,15 +100,41 @@ function getTimeToDisplay() {
         type: 'GET',
         url: uri + '/' + uniqueId,
         success: function (data) {
+            if(puckConnection != null) {
+                if (data.alert === true) {
+                    reactToAlert(puckConnection);
+                    //time.setMinutes(time.getMinutes() - 20);
+                }
+            }
             console.log('API TIME ' + data.newHours + ":" + data.newMinutes + ":" + data.newSeconds)
             hours = formatTimeDisplay(data.newHours);
             minutes = formatTimeDisplay(data.newMinutes);
             seconds = formatTimeDisplay(data.newSeconds);
 
-            // if (data.newSeconds >= 0 && data.newSeconds <=10) {
-            //     minutes = formatTimeDisplay(data.newMinutes - 20);
-            // } else {
-            //     minutes = formatTimeDisplay(data.newMinutes);                
+            // if (data.newSeconds === 0) {
+            //     hours = formatTimeDisplay(14);
+            //     minutes = formatTimeDisplay(20);
+            //     seconds = formatTimeDisplay(0);
+            // } else if (data.newSeconds === 1) {
+            //     hours = formatTimeDisplay(14);
+            //     minutes = formatTimeDisplay(0);
+            //     seconds = formatTimeDisplay(0);
+            // }else if (data.newSeconds === 2) {
+            //     hours = formatTimeDisplay(15);
+            //     minutes = formatTimeDisplay(20);
+            //     seconds = formatTimeDisplay(0);
+            // }else if (data.newSeconds === 3) {
+            //     hours = formatTimeDisplay(15);
+            //     minutes = formatTimeDisplay(0);
+            //     seconds = formatTimeDisplay(0);
+            // }else if (data.newSeconds === 4) {
+            //     hours = formatTimeDisplay(16);
+            //     minutes = formatTimeDisplay(20);
+            //     seconds = formatTimeDisplay(0);
+            // }else if (data.newSeconds === 5) {
+            //     hours = formatTimeDisplay(16);
+            //     minutes = formatTimeDisplay(0);
+            //     seconds = formatTimeDisplay(0);
             // }
 
             console.log('NEW TIME ' + hours + ":" + minutes + ":" + seconds)
