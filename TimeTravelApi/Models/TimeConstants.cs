38b50066b715e90b11f41f4f3d48eafda6d2ff08
claresudbery <clareworking@claresudbery.co.uk>
2018-10-28 04:16:40 +0000

    It can now handle multiple overlapping time requests from different users! (I think)

---
+++ TimeTravelApi/Models/TimeConstants.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace TimeTravelApi.Models
+{
+    public static class TimeConstants
+    {
+        // TODO: Change this to 20 minutes
+        public static int DefaultRequestLengthInMinutes = 1;
+    }
+}
