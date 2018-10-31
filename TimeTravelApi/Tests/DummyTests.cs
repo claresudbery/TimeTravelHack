18e29ce785c347b48da5150eeadb345759bcdbfa
claresudbery <clareworking@claresudbery.co.uk>
2018-10-31 15:16:25 +0000

    Added everything needed to run unit tests in this project.

---
+++ TimeTravelApi/Tests/DummyTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+namespace KiwilandRouteApplication.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void DemoTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
